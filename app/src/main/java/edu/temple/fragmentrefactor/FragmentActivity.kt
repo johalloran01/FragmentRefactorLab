6202c6bc7564ada20d2d7a4db012af9c87b65d01
johalloran01 <joshuamohalloran@gmail.com>
2023-10-25 15:37:43 -0400

    created a more fleshed out fragment xml file, and deleted main activity code.

--- app/src/main/java/edu/temple/fragmentrefactor/FragmentActivity.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/FragmentActivity.kt
@@ -11,6 +11,21 @@ class FragmentActivity : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setContentView(R.layout.fragment_main)
+
+        val displayTextView = findViewById<TextView>(R.id.displayTextView)
+        val nameEditText = findViewById<EditText>(R.id.nameEditText)
+        val changeButton = findViewById<Button>(R.id.changeButton)
+
+        changeButton.setOnClickListener {
+            val name = nameEditText.text
+
+            displayTextView.text = if (name.isNotBlank()) {
+                "Hello, $name!"
+            } else {
+                "Please enter your name"
+            }
+
     }
 
     override fun onCreateView(
@@ -18,8 +33,12 @@ class FragmentActivity : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activity, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
 
 }
+
+    private fun setContentView(fragmentActivity: Int) {
+
+    }
